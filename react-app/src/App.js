3387a3f33e3024678f8d8bf9e1ae3806ef7044b4
julianedwards <julian.edwards@mongodb.com>
2018-10-17 11:31:21 -0400

    add Authenticator to app.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Channel from './Channel';
 import './App.css';
+
+import Authenticator from './Authentiocator';
 import ChatService from './ChatService';
 
 const channel = {
@@ -17,6 +19,21 @@ class App extends Component {
     return (
       <div className="App">
         <Channel name={channel.name} chatService={this.chat} />
+        <header className="App-header" onClick={self.logMessages.bind(self)}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Authenticator />
       </div>
     );
   }
